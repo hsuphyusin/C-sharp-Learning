84432cb387f0590952c3e41026a74c0018017c61
hsuphyusin <alsahsu2017@gmail.com>
2022-10-02 11:30:47 +0630

    Testing for Homework question no.6

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -1,75 +1,62 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
+using System.Globalization;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
+using static System.Net.WebRequestMethods;
 
 namespace MyFirstProgram
 {
-
+    
 
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine( "\tSchool sumbit application");
+
             Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
 
-            String name = Console.ReadLine();
+            Console.WriteLine("What's your email address?");
+            string email = Console.ReadLine();
 
-            Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your phone number?");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What's your gender?");
             string gender = Console.ReadLine();
 
-            Console.WriteLine("What is your ID?");
-            int ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your address?");
+            string address = Console.ReadLine();
+            
+             /*Random random = new Random();
+            int IDnumber = random.Next(1,101);
+            Console.WriteLine("Your ID number is:" +IDnumber);*/
 
-            if (gender == "male")
+            if (address == "Mandalay")
             {
-                if (age >= 50)
-                {
-                    Console.WriteLine("You can't Sayy Htoe");
-                }
-                else if (age >= 18)
-                {
-                    Console.WriteLine("You can Sayy Htoe with 18mm.");
-                }
-                else if (age >= 10)
-                {
-                    Console.WriteLine("You can Sayy Htoe with 10mm.");
-                }
-                else if (age <= 0)
-                {
-                    Console.WriteLine("Impossible number.");
-                }
-                else if (age <= 10)
-                {
-                    Console.WriteLine("You can't Sayy Htoe.");
-                }
+               Console.WriteLine("You can submit your request.");
             }
             else
             {
-                if (ID >= 301)
-                {
-                    Console.WriteLine("You are Senior.");
-                }
-                else if (ID >= 101)
-                {
-                    Console.WriteLine("You are Junior.");
-                }
-                else if (ID < 100)
-                {
-                    Console.WriteLine("You are Senior.");
-                }
-                else if (ID < 0)
-                {
-                    Console.WriteLine("Your ID is invalid.");
-                }
+                Console.WriteLine("You can't submit request!);
+            } 
+             
+            int ID = 1;
+                                    
+            if (gender == "male")
+            {
+               Console.WriteLine(+2ID);
             }
-               
+            else
+            {
+               Console.WriteLine(++ID);
+            }  
             
 
-                /*string fullName = "Hsu Phyu Sin";
+               /*string fullName = "Hsu Phyu Sin";
                 string phoneNo = "123-456-789";
                 phoneNo = phoneNo.Replace("-","");
 
@@ -87,5 +74,8 @@ namespace MyFirstProgram
 
 
         }
+
+        
+
     }
 }
